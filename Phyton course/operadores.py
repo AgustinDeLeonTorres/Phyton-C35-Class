8bb47a37405acad89226d2635217ddeba6c5e010
AgustinDeLeonTorres <agustin.rafael.deleon@hotmail.com>
2025-07-23 19:54:00 -0600

    first commit

---
+++ Phyton course/operadores.py
@@ -0,0 +1,32 @@
+# print(2 + 2)
+# print(4 - 2)
+# print(3 * 7)
+# print(15 / 2)
+# print(11 % 4)
+# print(11 // 4)
+# print(2 ** 3)
+
+# print('hola' + 'mundo')
+# print('hola' * 3)
+# print('hola' + 'mundo' * 3)
+
+# print ('Hola' == 'hola')
+# print('Hola' != 'hola')
+# print(3 > 11)
+# print (11 >= 10)
+# print (10 <= 10)
+
+# print('ola' in 'Hola')
+# print('ola' not in 'Hola')
+
+# print(True or False)
+# print(True and False)
+
+# x = 1
+# x += 2
+# x *= 3
+# x %= 4
+# print (x)
+
+x = 6 * 5 + 8 / 2 ** 4
+print(x)
